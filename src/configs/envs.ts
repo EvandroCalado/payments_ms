715d3a6efa4a4d8e4a8aa88994328d83f247136e
Evandro Calado <evandrocalado07@gmail.com>
2024-12-22 17:32:26 -0300

    refactor: update NATS_SERVERS environment variable to support an array format

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -7,7 +7,7 @@ interface EnvVars {
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
-  NATS_SERVERS: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
@@ -17,11 +17,14 @@ const envVarsSchema = joi
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
-    NATS_SERVERS: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
